32a90604e5e2aed9eb85beee84a51e830557a389
khangdz2005k <khangdz2005k@gmail.com>
2025-09-27 19:14:49 +0700

    add ui manage product type

---
+++ quanly_nongsan/quanly_nongsan.py
@@ -0,0 +1,13 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+
+
+# from quanly_nongsan.login import index
+from quanly_nongsan.sidebar import index
+# from quanly_nongsan.phanloaihanghoa import index
+
+app = rx.App()
+app.add_page(index)
