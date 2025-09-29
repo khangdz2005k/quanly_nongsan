57d061285dafd4ab05046a17c584550a00dc11c9
khangdz2005k <khangdz2005k@gmail.com>
2025-09-29 11:05:57 +0700

    add: login

--- quanly_nongsan/quanly_nongsan.py
+++ quanly_nongsan/quanly_nongsan.py
@@ -13,46 +13,5 @@ load_dotenv()
 CONNECTION_STRING = os.getenv("DATABASE_URL")
 
 
-class State(rx.State):
-    """The app state."""
-    username: str = ""
-    password: str = ""
-    error_message: str = ""
-    def handle_login(self):
-        """Xử lý logic đăng nhập."""
-        self.error_message = "" # Reset thông báo lỗi
-        try:
-            with pyodbc.connect(CONNECTION_STRING) as conn:
-                cursor = conn.cursor()
-                # Tìm user dựa trên username
-                cursor.execute("SELECT PasswordHash FROM Users WHERE Username = ?", self.username)
-                user_row = cursor.fetchone()
-                if user_row and bcrypt.checkpw(self.password.encode(), user_row.PasswordHash.encode()):
-                    # ĐĂNG NHẬP THÀNH CÔNG!
-                    # Thay vì alert, chúng ta chuyển hướng đến trang dashboard
-                    return rx.redirect("/dashboard") 
-                else:
-                    # ĐĂNG NHẬP THẤT BẠI
-                    self.error_message = "Tên đăng nhập hoặc mật khẩu không đúng."
-
-        except Exception as e:
-            self.error_message = "Lỗi kết nối database."
-            print(f"Lỗi đăng nhập: {e}")
-
-    def set_username(self, value:str):
-        self.username = value
-        self.error_message = ""
-
-    def set_password (self, value:str):
-        self.password = value
-        self.error_message = ""
-
-def index() -> rx.Component:
-    return rx.center(
-        rx.text("Welcome to Quản lý nông sản"),
-    )
-
-
 app = rx.App()
-app.add_page(index, route="/")
 app.add_page(login_page, route="/login")
